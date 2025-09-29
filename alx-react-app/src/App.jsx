6cea1e80db957edbb80885f77cfcd5c1619c15ca
vjonah-sys <vanjonah045@gmail.com>
2025-09-29 18:10:45 +0100

    Remove old alx-react-app-new folder to restart project cleanly

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,25 +1,20 @@
 // src/App.jsx
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <>
-      <Header />
-      <MainContent />
       <UserProfile 
         name="Alice" 
         age="25" 
         bio="Loves hiking and photography" 
       />
+
       <UserProfile 
         name="Jonah" 
         age="30" 
         bio="A software developer who enjoys traveling" 
       />
-      <Footer />
     </>
   );
 }
