3919edf6275bc578ce76b33057c3b52bdeee051b
vjonah-sys <vanjonah045@gmail.com>
2025-09-29 17:00:46 +0100

    Add Userprofile.jsx

---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+// src/components/UserProfile.jsx
+
+function UserProfile(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
